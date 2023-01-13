37b7176d685602f8efa3385212d06f3fedfa9be5
milky <bakirrailya@gmail.com>
2023-01-13 14:36:13 +0300

    Init-react-app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Header from './Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      <h1>Абоба</h1>
+      <p>Pharagraph</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,30 @@
+// function Header() {
+//     return (
+//         <header>
+//             <h1>Header</h1>
+//             <p>This is a pharagraph</p>
+//         </header>
+//     );
+// }
+
+// export default Header;
+
+// export function Header() {
+//     return (
+//         <header>
+//             <h1>Header</h1>
+//             <p>This is a pharagraph</p>
+//         </header>
+//     );
+// }
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Header</h1>
+            <p>This is a pharagraph</p>
+        </header>
+    );
+}
+
+export default Header;
